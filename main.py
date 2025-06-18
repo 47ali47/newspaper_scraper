b2f180eddab6c73899e08de0341b09a99f280dda
47ali47 <anbo.li843@gmail.com>
2025-06-17 22:24:23 -0700

    imports

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+#  Imports
+import requests # allows access to webpages via HTTP
+from bs4 import BeautifulSoup # parses HTML to extract structured data
+from langchain.text_splitter import RecursiveCharacterTextSplitter # splits text into chunks for storage
+from langchain.schema import Document # stores text and URL
