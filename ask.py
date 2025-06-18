a7e6b55c7d1404c73d06550d565f1d886e0a8686
47ali47 <anbo.li843@gmail.com>
2025-06-17 22:02:28 -0700

    deleted ask.py

--- ask.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import TextLoader
-from langchain.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.chains import RetrievalQA
-from langchain_community.embeddings import OpenAIEmbeddings
-
