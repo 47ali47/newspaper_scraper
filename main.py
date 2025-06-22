7f8d0aff98b36df946e711865a9f0304ef0de81b
47ali47 <anbo.li843@gmail.com>
2025-06-21 18:04:40 -0700

    turned shell script into python

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 # alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 # chrome --headless --disable-gpu --print-to-pdf https://www.columbiaspectator.com/news/2025/06/16/khalils-first-direct-statement-to-the-court-released/
 
+# Imports to convert shell code into python code
+import subprocess
+import argparse
+
 from pypdf import PdfReader
 from text_analyzer import text_analyzer
 
@@ -12,11 +16,43 @@ def extract_text(pdf):
     output = "\n".join(texts)
     return output
 
+# Convert a URL into a PDF 
+def urlToPDF(
+        url: str,
+        output: str,
+        chrome_path: str = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"):
+    parser = argparse.ArgumentParser(
+    )
+    
+    cmd = [
+        chrome_path,
+        "--headless",
+        "--disable-gpu",
+        f"--print-to-pdf={output}",
+        url
+    ]
+
+    print("CMDDDD", cmd)
+
+    try:
+        result = subprocess.run(
+            cmd, 
+            check=True, 
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+    except subprocess.CalledProcessError:
+        print("Failed to generate PDF")
+    
+
 if __name__ == "__main__":
+    # important to have .pdf in the name of the otput
+    urlToPDF(url="https://www.columbiaspectator.com/opinion/2024/05/16/the-palestine-exception/", output="article_1.pdf")
+
     # PDF of newspaper is converted into a string with its text
-    extracted_text = extract_text("palestine_exception_article.pdf")
+    # extracted_text = extract_text("palestine_exception_article.pdf")
     # the text is analyzed by LLM acting as researcher
-    print(text_analyzer(extracted_text))
+    # print(text_analyzer(extracted_text))
 
     
 
